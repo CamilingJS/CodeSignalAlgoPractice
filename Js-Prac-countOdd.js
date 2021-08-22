1edef22006796cc3d484b11c06b5f716f874e947
Jonathan <jonathancamiling@gmail.com>
2021-08-22 15:55:54 -0700

    added solution to countOdd

---
+++ Js-Prac-countOdd.js
@@ -0,0 +1,26 @@
+// 🤔 countOdd
+// Given a number, count how many odd numbers are below that number
+// Ex: countOdd(5) -> 1, 3 -> returns 2
+// Ex: countOdd(10) -> 1, 3, 5, 7, 9 -> returns 5
+// 0 / 3 Tests Passed
+// ❌ countOdd(5) // It counts up to, but not including an odd num
+// expected 2 got undefined
+// ❌ countOdd(10) // It also works for an even num
+// expected 5 got undefined
+// ❌ countOdd(1) // It is zero unless at least 2
+// expected 0 got undefined
+
+let n = 13;
+
+var countOdd = function(n) {
+  let oddCount = 0; 
+  while (n--){
+    n-1; 
+    if (n%2 !=0){
+      oddCount++; 
+    }
+  }
+  return oddCount; 
+  
+}
+console.log(countOdd(n));
