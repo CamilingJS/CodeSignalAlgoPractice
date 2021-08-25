d71b80f36eaa2a1d8e3ff34d29e57d00562ca892
Jonathan <jonathancamiling@gmail.com>
2021-08-24 23:43:38 -0700

    initiated Pract-arrayToObjectV2

---
+++ JS_Pract-arrayToObjectV2.js
@@ -0,0 +1,8 @@
+const users = [ 
+  {username: 'aaron', fullName: 'Aaron Jack', description: 'Software developer'},
+  {username: 'jan', fullName: 'Jan Frey', description: 'Shopify developer'},
+  {username: 'igor', fullName: 'Igor Kalishnakov', description: 'Massage therapist'},
+]
+
+// {username: full user object}
+
