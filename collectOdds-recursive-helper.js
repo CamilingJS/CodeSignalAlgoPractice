6795861413be3ca65a7501b9345224760dba0f65
Jonathan <jonathancamiling@gmail.com>
2021-10-20 00:17:39 -0700

    modified collectOddValues1 as straight recursive

--- collectOdds-recursive-helper.js
+++ collectOdds-recursive-helper.js
@@ -1,7 +1,7 @@
 
 function collectOddValues(arr){
   let result = [];
-  
+
   function helper(helperInput){
     if(helperInput.length === 0){
       return; 
@@ -15,3 +15,17 @@ function collectOddValues(arr){
   return result; 
 }
 console.log(collectOddValues([1,2,3,4,5,6,7,8,9]))
+
+//pure recursive method
+function collectOddValues1 (arr){
+  let newArr = [];
+  if(arr.length === 0){
+    return newArr; 
+  }
+  if(arr[0] % 2 !==0){
+    newArr.push(arr[0])
+  }
+  newArr = newArr.concat(collectOddValues1(arr.slice(1)))
+  return newArr; 
+}
+console.log(collectOddValues1([1,2,3,4,5,6,7,8,9]))
