a3434eda6920535f33e3aa7010574d0f1e3a439a
Jonathan <jonathancamiling@gmail.com>
2021-10-19 22:19:49 -0700

    very simple recursive function that counts down

---
+++ recursiveEx1.js
@@ -0,0 +1,13 @@
+// simple count down function
+
+function countDown (num){
+  if (num <=0 ){
+    console.log("All done!");
+    return; 
+  }
+  console.log(num)
+  num --;
+  countDown(num)
+}
+
+console.log(countDown(10));
