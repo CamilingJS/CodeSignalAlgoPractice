dfa2b663dd102eb3761a178e2863a1622c7858af
Jonathan <jonathancamiling@gmail.com>
2021-10-29 23:48:32 -0700

    made progress for mostFrequent

--- mostFrequent.js
+++ mostFrequent.js
@@ -1,24 +1,25 @@
 //fn that creates most frequent word from a string
 //where it's not in a banned list 
 
-let paragraph = "A bit of this that that this that at"
+let paragraph = "A little little bit of this a little bit of that and that is where it is at"
 let banned = ["that"] 
 
-const mostFreq = (paragraph)=>{
+const mostFreq = (paragraph, banned)=>{
   let newArr = paragraph.toLowerCase().split(" ") 
   let obj = {}; 
+  console.log(newArr)
+
   for (let i=0; i<newArr.length; i++){
-    let currStr = newArr[i]
-    obj[currStr] ? obj[currStr]++ : obj[currStr]=1; 
+      let currStr = newArr[i]
+      obj[currStr] ? obj[currStr]++ : obj[currStr]=1; 
     
   }
   console.log(obj)
-  for (let Key in obj){
-    Object.values(obj).filter((element)=>{
-      Math.max(element)
-    })
-    return obj
-  }
+  
+  let res = Object.keys(obj).reduce((a,b) => obj[a] > obj[b] ? a:b)
+  console.log(res)
+
+  return res
 }
 
 console.log(mostFreq(paragraph))
