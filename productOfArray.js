4aa2a747837af3e6b7909e91a9ede84073caa7ee
Jonathan <jonathancamiling@gmail.com>
2021-11-03 01:36:43 -0700

    initaited productOfArray

---
+++ productOfArray.js
@@ -0,0 +1,8 @@
+//Write a function called productOfArray which takes an 
+//array of numbers and returns the product of them all
+
+function productOfArray(){
+
+}
+
+console.log(productOfArray([1,2,3]))
