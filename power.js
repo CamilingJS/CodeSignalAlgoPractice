08c0cf23851084734cc467970b299a4d273aeb78
Jonathan <jonathancamiling@gmail.com>
2021-11-02 23:48:39 -0700

    initiated power.js

---
+++ power.js
@@ -0,0 +1,10 @@
+//Write a fn which accepts a base and an exponent
+//The fn should return the power of the base to the exponent
+//This fn should mimic the functionality of Math.pow()
+// do not worry about negative bases and exponents
+
+function power(base, exp){
+  return base ** exp
+}
+
+console.log(power(2,4))
