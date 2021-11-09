a6c854955d056cb32d1981ee94e0fa3d67606426
Jonathan <jonathancamiling@gmail.com>
2021-11-08 23:24:10 -0800

    attempted productOfArray - status unfinished

--- productOfArray.js
+++ productOfArray.js
@@ -2,7 +2,11 @@
 //array of numbers and returns the product of them all
 
 function productOfArray(){
-
+  for(let i=0; i<arguments.length; i++){
+    return product = arguments[i] * arguments[i+1]
+    
+  }
+  console.log(product)
 }
 
 console.log(productOfArray([1,2,3]))
