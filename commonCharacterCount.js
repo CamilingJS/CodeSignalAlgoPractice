89322f6edbfb27462504b612decb4f078d74e493
CamilingJS <jonathancamiling@gmail.com>
2022-07-12 22:43:02 -0700

    completed commonCharacterCount. I created an object to include the first string, and as I loop through the 2nd string if the letter is already in the object I count++ and decease that string count in the obj

--- commonCharacterCount.js
+++ commonCharacterCount.js
@@ -1,22 +1,22 @@
 
-let s1 = "aabcc"
-let s2 = "adcaa"
+let s1 = "zzzz"
+let s2 = "zzzzzzz"
 function commonCharacterCount(s1, s2){
   let obj = {}
+  let counter = 0; 
   for(let i = 0; i<s1.length; i++){
     let letter = s1[i]
-    obj[letter] ? obj[letter]++ : obj[letter]=1; 
+    obj[letter] ? obj[letter]++ : obj[letter] =1; 
   }
-  console.log(obj)
 
-
-  for (let i =0; i<s2.length; i++){
+  for(let i = 0; i<s2.length; i++){
     let letter = s2[i]
-    obj[letter] ? obj[letter]++ : obj[letter]=1; 
+    if (obj[letter]){
+      counter++
+      obj[letter]--
+    }
   }
-  let count = Object.values(obj)
-  console.log(count)
-  console.log(obj)
+  return counter; 
 }
 
 console.log(commonCharacterCount(s1, s2))
