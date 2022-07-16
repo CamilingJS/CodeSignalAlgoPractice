a7c30ef8088030aa1e87454f0698a02af9114d56
CamilingJS <jonathancamiling@gmail.com>
2022-07-15 19:57:50 -0700

    initiated addBorder challenge

---
+++ addBorder.js
@@ -0,0 +1,44 @@
+// Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+
+// Example
+
+// For
+
+// picture = ["abc",
+//            "ded"]
+// the output should be
+
+// solution(picture) = ["*****",
+//                       "*abc*",
+//                       "*ded*",
+//                       "*****"]
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] array.string picture
+
+// A non-empty array of non-empty equal-length strings.
+
+// Guaranteed constraints:
+// 1 ≤ picture.length ≤ 100,
+// 1 ≤ picture[i].length ≤ 100.
+
+// [output] array.string
+
+// The same matrix of characters, framed with a border of asterisks of width 1.
+
+picture = ["abc",
+           "ded"]
+
+function solution(picture) {
+    let width = picture[0].length; 
+    
+    let firstRow = ""
+    let n = 0; 
+    while(n<width){
+        firstRow + "*"
+    }
+    console.log(firstRow)
+}
+console.log(solution(picture))
