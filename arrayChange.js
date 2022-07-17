6938fa62fb470bcf2a3fddf7afb0fe91984ce9d7
CamilingJS <jonathancamiling@gmail.com>
2022-07-17 13:13:25 -0700

    initiated arrayChange challenge

---
+++ arrayChange.js
@@ -0,0 +1,41 @@
+// You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
+// Example
+
+// For inputArray = [1, 1, 1], the output should be
+// solution(inputArray) = 3.
+
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] array.integer inputArray
+
+// Guaranteed constraints:
+// 3 ≤ inputArray.length ≤ 105,
+// -105 ≤ inputArray[i] ≤ 105.
+
+// [output] integer
+
+// The minimal number of moves needed to obtain a strictly increasing sequence from inputArray.
+// It's guaranteed that for the given test cases the answer always fits signed 32-bit integer type.
+
+//i=1, if num -1 === num[i-1] then i++
+//let moves = (num - num[i-1])
+// if num -1 !==num[i-1]  then num = moves + num 
+
+let inputArray = [5, 4, 3, 2, 1];
+function solution(inputArray) {
+
+    let count = 0;
+
+    for(let i = 0; i<inputArray.length; i++){
+        const num = inputArray[i];
+        let priorNum = inputArray[i-1]
+        console.log(num)
+        console.log(priorNum)
+        if(num-1 === inputArray){
+
+        }
+    }
+}
