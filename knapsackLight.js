da7f3a4c173148ef54e866279dbf334299e38df6
CamilingJS <jonathancamiling@gmail.com>
2022-08-17 23:24:23 -0700

    Initiated knapsackLight challenge

---
+++ knapsackLight.js
@@ -0,0 +1,86 @@
+// You found two items in a treasure chest! The first item weighs weight1 and is worth value1, and the second item weighs weight2 and is worth value2. What is the total maximum value of the items you can take with you, assuming that your max weight capacity is maxW and you can't come back for the items later?
+
+// Note that there are only two items and you can't bring more than one item of each type, i.e. you can't take two first items or two second items.
+
+// Example
+
+// For value1 = 10, weight1 = 5, value2 = 6, weight2 = 4, and maxW = 8, the output should be
+// solution(value1, weight1, value2, weight2, maxW) = 10.
+
+// You can only carry the first item.
+
+// For value1 = 10, weight1 = 5, value2 = 6, weight2 = 4, and maxW = 9, the output should be
+// solution(value1, weight1, value2, weight2, maxW) = 16.
+
+// You're strong enough to take both of the items with you.
+
+// For value1 = 5, weight1 = 3, value2 = 7, weight2 = 4, and maxW = 6, the output should be
+// solution(value1, weight1, value2, weight2, maxW) = 7.
+
+// You can't take both items, but you can take any of them.
+
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] integer value1
+
+// Guaranteed constraints:
+// 2 ≤ value1 ≤ 20.
+
+// [input] integer weight1
+
+// Guaranteed constraints:
+// 2 ≤ weight1 ≤ 10.
+
+// [input] integer value2
+
+// Guaranteed constraints:
+// 2 ≤ value2 ≤ 20.
+
+// [input] integer weight2
+
+// Guaranteed constraints:
+// 2 ≤ weight2 ≤ 10.
+
+// [input] integer maxW
+
+// Guaranteed constraints:
+// 1 ≤ maxW ≤ 20.
+
+// [output] integer
+
+
+let value1 = 15
+let weight1 = 2
+let value2 = 20
+let weight2 = 3
+let maxW = 2
+
+function solution(value1, weight1, value2, weight2, maxW) {
+    let totalItems = 0
+
+    if (weight1 > maxW && weight2> maxW){
+        return totalItems
+    }
+
+    if(weight1 + weight2 <= maxW){
+        totalItems += value1+value2
+    }
+    if(weight1 + weight2 > maxW && weight1>weight2  && weight1==maxW){
+        totalItems+= value1
+    }
+
+    if(weight1 + weight2 > maxW && weight2>weight1  ){
+        totalItems+= value2
+    }
+
+    
+
+    return totalItems 
+
+}
+
+
+console.log(solution(value1, weight1, value2, weight2, maxW) )
+
