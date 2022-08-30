463e489320d304691991d8464a6b20906525ad77
CamilingJS <jonathancamiling@gmail.com>
2022-08-30 00:13:18 -0700

    Found way to create a dictionary out of the fruits array

---
+++ introToHashOrDictionary.js
@@ -0,0 +1,73 @@
+const fruits = [
+    {
+      priceInCents: 700, // 0 in JavaScript is falsy
+      name: 'Coconut'
+    },
+    {
+      priceInCents: 100,
+      name: 'Blueberry'
+    },
+    {
+      priceInCents: 100,
+      name: 'Apple'
+    },
+    {
+      priceInCents: 100,
+      name: 'Pineapple'
+    },
+    {
+      priceInCents: 100,
+      name: 'Orange'
+    },
+    {
+      priceInCents: 1000,
+      name: 'Orange'
+    },
+    {
+      priceInCents: 150,
+      name: 'Grapes'
+    },
+    {
+      priceInCents: 1000,
+      name: 'Grapefruit'
+    },
+    {
+      priceInCents: 500,
+      name: 'Mango'
+    },
+    {
+      priceInCents: 1500,
+      name: 'Papaya'
+    },
+  ]
+
+
+
+function fruitDict(fruits){
+    const fruitMap = {};
+    for (let fruit of fruits)
+    {
+        if(fruitMap[fruit])
+        {
+            fruitMap[fruit.name].push(fruit.priceInCents)
+        } else
+        {
+            fruitMap[fruit.name] = fruit.priceInCents
+        }
+    }
+    return fruitMap
+}
+console.log(fruitDict(fruits))
+
+  const fruitDictionary = fruitDict(fruits)
+
+
+
+  
+
+
+
+
+  
+
+
