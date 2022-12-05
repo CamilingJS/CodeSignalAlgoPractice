cbe06459fd496ca73add4d363dbd0c645b7c73a7
CamilingJS <jonathancamiling@gmail.com>
2022-12-04 16:55:04 -0800

    Created a function that groups the anagrams of an array of strings so that each anagrams are in their own array

--- anagramsGroup.js
+++ anagramsGroup.js
@@ -1,16 +1,16 @@
 
-//Create a function that groups the anagrams of an array of strings so that each anagrams are in their own array
+//Created a function that groups the anagrams of an array of strings so that each anagrams are in their own array
 
 const arr = ['jonathan', 'onathanj', 'john', 'ohnj', 'joy', 'yoj']
 let groupAnagrams = function (arr){
     const ht = {}
     for(let str of arr){
-        const sorted = str.split('').sort().join()
+        const sortedStr = str.split('').sort().join()
 
         //if sorted array already exist then push another str in that sorted array
-        if(ht[sorted]) ht[sorted].push(str)
+        if(ht[sortedStr]) ht[sortedStr].push(str)
 
-        else ht[sorted] = [str]
+        else ht[sortedStr] = [str]
         
     }
 
