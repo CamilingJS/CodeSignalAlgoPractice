0af6d9f24244911249b4371ed31bd0daba251c27
CamilingJS <jonathancamiling@gmail.com>
2024-08-07 23:42:25 -0700

    args practice

--- python_algos/main.py
+++ python_algos/main.py
@@ -306,21 +306,83 @@ peak["elevation"] = peak.pop("height")
 
 
 # Loop over the values in the dictionary and print them all out.  Don't ask why, just do it :) 
-# arrVal = peak.values()
-# for value in arrVal:
+
+# for value in peak.values():
 #     print(value)
 
 
 # Loop over the keys AND values in the dictionary and print them all out in the following format:
 # key -> value
 # (print an arrow between each pair)
-
-arr = peak.items()
-for key, val in arr:
-    print(f"{key} --> {val}")
+# for key, val in peak.items():
+    # print(f"{key} --> {val}")
 
 
 # A huge earthquake/meteor/forestfire/tsunami has destroyed the peak.  Please empty out the entire dictionary.
-peak.clear()
+# peak.clear()
 
-print("Post armagaedon", peak)
+# print("Post armagaedon", peak)
+
+# def add_twice(val, lst = None):
+#     if lst is None:
+#         lst = []
+#     lst.append(val)
+#     lst.append(val)
+#     return lst
+
+# print(add_twice("Tan"))
+
+# ***********************************************************************
+
+# ============== PART 1 ============== 
+# Write a function called contains_pickle that accepts any number of arguments. 
+# The function should return True if it is passed "pickle" as one of the args
+# Otherwise it should return False
+
+#  contains_pickle("red", 45, "pickle", [])  --> True
+#  contains_pickle(1,2, "blue") ---------------> False
+
+def contains_pickle(*args):
+    return "pickle" in args
+
+# print(contains_pickle(1,2, "blue"))
+    
+# ============== PART 2 ============== 
+# Write a function called count_fails that counts up the number of failing test scores it is passes
+# It should accept any number of arguments
+# It should return a count of how many args are less than or equal to 50
+
+# count_fails(99,48,79,36) -------> 2
+# count_fails(85,78,91) ----------> 0
+# count_fails(50,41,47,74,76,81) -> 3
+
+def count_fails(*args):
+    count = 0
+    for arg in args:
+        if arg < 51:
+            count += 1
+    return count
+
+# print(count_fails(99,48,79,36))
+# print(count_fails(85,78,91))
+# print(count_fails(50,41,47,74,76,81))
+
+# ============== PART 3 ============== 
+# Write a function called get_top_students that helps teachers find their A-grade students!
+# It should accept any number of student=score keyword arguments like colt=78 or elton=98
+# It should return a list containing the names of students who scored 90 or above
+
+# get_top_students(tim=91, stacy=83, carlos=97, jim=69) -> ['tim', 'carlos']
+# get_top_students(colt=61, elton=76) -------------------> []
+# get_top_students(kitty=80, blue=95, toad=91)-----------> ['blue', 'toad']
+
+def get_top_students(**kwargs):
+    arr = []
+    for k, v in kwargs.items():
+        if v > 89:
+            arr.append(k)
+    return arr
+
+# print(get_top_students(tim=91, stacy=83, carlos=97, jim=69))
+# print(get_top_students(colt=61, elton=76))
+# print(get_top_students(kitty=80, blue=95, toad=91))
