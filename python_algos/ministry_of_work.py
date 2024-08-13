9cff0ebea9504681d48f74e0ef8a5688aa36373d
CamilingJS <jonathancamiling@gmail.com>
2024-08-12 23:28:04 -0700

    Sentiment analysis

--- python_algos/ministry_of_work.py
+++ python_algos/ministry_of_work.py
@@ -1,11 +1,20 @@
 from textblob import TextBlob
+import pyttsx3
 
-print("Enter your employee wellness statemet:  ")
+engine = pyttsx3.init()
+engine.say("Hello Employee Number 45621. We hpe you had a great day of work.")
+engine.runAndWait()
+
+engine.say("Enter your employee wellness statement:  ")
+engine.runAndWait()
 phrase = input(">  ")
 blob = TextBlob(phrase)
+engine.say({phrase})
+engine.runAndWait()
 while blob.sentiment.polarity < 0.5:
-    print("Be more positive please: ")
+    engine.say("Be more positive please: ")
     phrase = input(">  ")
     blob = TextBlob(phrase)
 
-print("Yayy thanks!")
+engine.say("Yayy thanks! You can now go home!")
+engine.runAndWait()
